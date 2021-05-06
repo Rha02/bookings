bd09a2e24aa2dc7b6ff46e7f2c241cb6df7e7cf8
Romuz <romuzus08@gmail.com>
2021-05-06 15:14:18 -0700

    Created Database Seeders

---
+++ migrations/20210506215833_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2021-05-01 00:00:00.000','2021-05-01 00:00:00.000'),
+	 ('Colonel''s Suite','2021-05-01 00:00:00.000','2021-05-01 00:00:00.000');
---
+++ migrations/20210506220954_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2021-05-01 00:00:00.000','2021-05-01 00:00:00.000'),
+	 ('Owner Block','2021-05-01 00:00:00.000','2021-05-01 00:00:00.000');
