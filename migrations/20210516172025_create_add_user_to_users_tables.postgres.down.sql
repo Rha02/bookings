0434fd1f8c52152289de9f7c8a085ee098ca92dd
Romuz <romuzus08@gmail.com>
2021-05-16 10:40:03 -0700

    Added User seeder

---
+++ migrations/20210516172025_create_add_user_to_users_tables.postgres.down.sql
@@ -0,0 +1,1 @@
+delete from users where email = "admin@bookings.loc"
---
+++ migrations/20210516172025_create_add_user_to_users_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (email,first_name,last_name,"password",access_level,created_at,updated_at) VALUES
+	 ('admin@bookings.loc','Admin','Adminovsky','$2a$12$DkLLLLs14RFOnQIcnupWguVE7w2JLTKYinGqzsSoBl8xT8VC1hD1S',3,'2021-05-10 00:00:00.000','2021-05-10 00:00:00.000');
